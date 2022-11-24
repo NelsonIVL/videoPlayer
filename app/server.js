ecb75d769f570d076ea182de64b611b51d1c389e
NelsonIVL <is708203@iteso.mx>
2022-11-24 16:45:51 -0600

    Server

---
+++ app/server.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const express = require('express');
+const cors = require('cors');
+const router = require('./controllers/router');
+
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use(router);
+
+app.listen(port,() => {
+    console.log(`Example app listening on port ${port}`);
+});
