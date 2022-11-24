1394ccd3187b92dcfa2a927b506896f4ae4bd5d5
NelsonIVL <is708203@iteso.mx>
2022-11-24 16:42:40 -0600

    Creación de la clase payment

---
+++ app/controllers/payment.js
@@ -0,0 +1,13 @@
+"use strict";
+
+class Payment{
+
+    constructor(idUser, noTarjeta, fechaVencimiento, codigoS){
+        this.idUser = idUser;
+        this.noTarjeta = noTarjeta;
+        this.fechaVencimiento = fechaVencimiento;
+        this.codigoS = codigoS;
+    }
+
+    
+}
